5662aa7411b138612f8557eabee3ca4f348dc96d
indra <indra1nkuss@gmail.com>
2026-02-27 21:08:55 +0700

    menambahkan halaman keranjang

--- lib/main.dart
+++ lib/main.dart
@@ -99,3 +99,46 @@ class AddButton extends StatelessWidget {
     );
   }
 }
+
+// Halaman Keranjang
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // context.watch membuat widget
+    // ini re-build tiap kali CartModel berubah
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Keranjang Belanja')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: const Icon(Icons.fastfood),
+                title: Text(cart.items[index]),
+              ),
+            ),
+          ), // Expanded
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeAll(),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text(
+                'Hapus Keranjang',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ), // Padding
+        ],
+      ), // Column
+    ); // Scaffold
+  }
+}
