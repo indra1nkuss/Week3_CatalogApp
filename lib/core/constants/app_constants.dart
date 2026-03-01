cc8d4491dc8211da02da79e865708aff537b82fd
indra <indra1nkuss@gmail.com>
2026-03-01 20:41:33 +0700

    menambahkan code untuk menyimpan data konstan

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+//Tempat menyimpan data konstan agar tidak di-hardcode di UI.
+
+class AppConstants {
+  static const List<String> catalogItems = [
+    'Nasi Goreng', 'Sate Ayam', 'Es Teh', 'Ayam Bakar', 'Kopi'
+  ];
+}
