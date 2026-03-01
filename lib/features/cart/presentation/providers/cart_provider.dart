7b556908dccbfcb2e45d1842fbc863168f5fde70
indra <indra1nkuss@gmail.com>
2026-03-01 21:24:37 +0700

    menambahkan code Penghubung antara UI dan Data.

---
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../domain/entities/product.dart';
+import '../../domain/repositories/cart_repository.dart';
+
+class CartProvider extends ChangeNotifier {
+  final CartRepository repository;
+
+  CartProvider({required this.repository});
+
+  List<Product> get items => repository.getItems();
+
+  void add(Product product) {
+    repository.addItem(product);
+    notifyListeners(); // Update UI
+  }
+
+  void removeAll() {
+    repository.clearCart();
+    notifyListeners(); // Update UI
+  }
+
+  bool isItemInCart(String productName) {
+    return items.any((item) => item.name == productName);
+  }
+}
